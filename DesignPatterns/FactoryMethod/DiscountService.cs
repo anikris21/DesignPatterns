56c054aede380655e41e2f11fd64188b1e1f7bed
Anil Kumar K K <akumarkk@outlook.com>
2023-07-30 20:02:23 +0000

    factory method

---
+++ DesignPatterns/FactoryMethod/DiscountService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public  abstract class DiscountService
+    {
+        public abstract int DiscountPercentage { get; }
+        public override string ToString()
+        {
+            return GetType().Name;
+        }
+    }
+
+    public class CountryDiscountService : DiscountService
+    {
+        private readonly string _countryIdentifier;
+        public CountryDiscountService(string countryId)
+        {
+            _countryIdentifier = countryId;
+        }
+
+        public override int DiscountPercentage
+        {
+            get
+            {
+                switch(_countryIdentifier)
+                {
+                    case "US": return 20;
+
+                    case "ME": return 10;
+                    default: return 10;
+                }
+            }
+
+        }
+    }
+
+    public class CodeDiscountService : DiscountService
+    {
+        private readonly Guid _code;
+        public CodeDiscountService(Guid code)
+        {
+            _code = code;
+        }
+
+        public override int DiscountPercentage
+        {
+            get
+            {
+                return 15;
+            }
+        }
+    }
+
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();
+    }
+
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryId;
+        public CountryDiscountFactory(string countryId) { _countryId = countryId; }
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryId);
+        }
+    }
+
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        private readonly Guid _code;
+        public CodeDiscountFactory(Guid code) { _code = code; }
+        public override DiscountService CreateDiscountService()
+        {
+            return new CodeDiscountService(_code);
+        }
+    }
+}
---
+++ DesignPatterns/FactoryMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿using FactoryMethod;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Title = "Factory method";
+
+var factories = new List<DiscountFactory> {
+new CodeDiscountFactory(Guid.NewGuid()), new CountryDiscountFactory("US")
+};
+
+foreach (var factory in factories)
+{
+    var discountService = factory.CreateDiscountService();
+    Console.WriteLine($"%   {discountService.DiscountPercentage} coming from {discountService}");
+}
