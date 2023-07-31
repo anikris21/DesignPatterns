4755adb7610be920c6a11f59e00ba94ea543ff10
Anil Kumar K K <akumarkk@outlook.com>
2023-07-31 18:46:25 +0000

    singleton

---
+++ DesignPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,9 @@
+﻿using SingletonPattern;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+UserManager u1 = UserManager.Instance;
+
+Console.WriteLine(u1.st);
+
+Console.ReadKey();
---
+++ DesignPatterns/SingletonPattern/UserManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class UserManager
+    {
+        private UserManager() {
+            st = "user manager loaded";
+        }
+        public string st;
+        
+        private static readonly Lazy<UserManager> instance = new Lazy<UserManager>(() => { return new UserManager(); });
+        public static UserManager Instance => instance.Value;
+    }
+}
