ba64edc8a2cf2eb0e251bf2a056f0469720c2a96
Anil Kumar K K <akumarkk@outlook.com>
2023-07-31 16:22:32 +0000

    prototype

---
+++ DesignPatterns/PrototypePattern/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PrototypePattern;
+
+Console.WriteLine("Hello, World!");
+
+User user = new User("Avin", "Blore");
+user.Age = 30;
+
+User u1 = user.Clone() as User;
+Console.WriteLine(u1.ToString());
---
+++ DesignPatterns/PrototypePattern/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+
+    class User: IUser
+    {
+        public User(string name, string addr) {
+
+            this.name = name;
+            this.Address = addr;
+        }
+        private string name;
+        public string Name {
+            get
+            {
+                return name;
+            }
+        }
+        public string Address { get; set; }
+        private int age; 
+        public int Age { 
+        set
+            {
+                age = value;
+            }
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return  $"{name}    {age}   {Address}";
+        }
+
+    }
+    interface IUser :ICloneable
+    {
+        string Address { get; set; }
+        int Age { set; }
+        string Name { get; }
+    }
+
+
+
+}
