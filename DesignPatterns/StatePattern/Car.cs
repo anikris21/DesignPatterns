6a07e52195e5cd2a4390f70e14c5d84e0ab7a990
Anil Kumar K K <akumarkk@outlook.com>
2023-08-02 03:37:08 +0000

    state pattern

---
+++ DesignPatterns/StatePattern/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+     class Car
+    {
+        // 
+        public State state = State.Stopped;
+
+        public State CurrentState { get { return state; } }
+
+        public enum State
+        {
+            Stopped,
+            Started,
+            Running
+        }
+        public enum Action
+        {
+            Start,
+            Stop,
+            Accelarate
+        }
+
+        public void TakeAction(Action action)
+        {
+            switch ((state, action))
+            {
+                case (State.Stopped, Action.Start): state = State.Started; break;
+                case (State.Started, Action.Accelarate): state = State.Running; break;
+                case (State.Started, Action.Stop): state = State.Stopped; break;
+                case (State.Running, Action.Stop): state = State.Stopped; break;
+                default: break;
+            }
+        }
+    }
+}
---
+++ DesignPatterns/StatePattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using StatePattern;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Car car = new Car();
+Console.WriteLine($"state = ${car.CurrentState}");
+car.TakeAction(Car.Action.Start);
+Console.WriteLine($"state = ${car.CurrentState}");
+car.TakeAction(Car.Action.Accelarate);
+Console.WriteLine($"state = ${car.CurrentState}");
