3817b002515db94ce36c47deeb3205c0e9547a07
Anil Kumar K K <akumarkk@outlook.com>
2023-08-03 02:29:37 +0000

    weather provider lib

---
+++ DesignPatterns/WeatherforecastLibrary/IWeatherProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherforecastLibrary
+{
+    public interface IWeatherProvider
+    {
+        IEnumerable<Weatherforecast> Get(int totalCount);
+    }
+}
---
+++ DesignPatterns/WeatherforecastLibrary/WeatherProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherforecastLibrary
+{
+    public class WeatherProvider: IWeatherProvider
+    {
+        public readonly string[] Summaries = new string[] {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        // 
+        public IEnumerable<Weatherforecast> Get(int totalCount)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, totalCount).Select(i =>
+            new Weatherforecast
+            {
+                Date = DateTime.Now.AddDays(i),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            });
+        }
+    }
+}
